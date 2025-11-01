b9e341b8ff84bcc7bc1d98f1b7562c770c905e8d
tongkat-sakti-penjinak-janda <tongkat-sakti@penjinak-janda.com>
2025-11-01 13:34:52 +0700

    Project initial commit.

---
+++ app/src/main/java/com/example/reply/ui/MainActivity.kt
@@ -0,0 +1,60 @@
+package com.example.reply.ui
+
+import android.content.res.Configuration.UI_MODE_NIGHT_NO
+import android.content.res.Configuration.UI_MODE_NIGHT_YES
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.viewModels
+import androidx.compose.material3.Surface
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.reply.data.LocalEmailsDataProvider
+import com.example.reply.ui.theme.AppTheme
+
+
+class MainActivity : ComponentActivity() {
+    private val viewModel: ReplyHomeViewModel by viewModels()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        setContent {
+            val uiState by viewModel.uiState.collectAsState()
+
+            AppTheme {
+                Surface {
+                    ReplyApp(
+                        replyHomeUIState = uiState,
+                        closeDetailScreen = {
+                            viewModel.closeDetailScreen()
+                        },
+                        navigateToDetail = { emailId ->
+                            viewModel.setSelectedEmail(emailId)
+                        }
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Preview(
+    uiMode = UI_MODE_NIGHT_YES,
+    name = "DefaultPreviewDark"
+)
+@Preview(
+    uiMode = UI_MODE_NIGHT_NO,
+    name = "DefaultPreviewLight"
+)
+@Composable
+fun ReplyAppPreviewLight() {
+    ReplyApp(
+        replyHomeUIState = ReplyHomeUIState(
+            emails = LocalEmailsDataProvider.allEmails
+        )
+    )
+}
